1af1944b08a4448c84d15ed1c8ce4c263a879df9
dmitry-mak <akkaru@gmail.com>
2025-02-27 13:49:04 +0200

    Tests added

---
+++ src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
@@ -0,0 +1,36 @@
+package ru.netology.postmanecho;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void shouldReturnSomeData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Some data"));
+    }
+
+    @Test
+    public void headersTest() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .header("Custom-header", "Custom value")
+                .body("Some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("headers.custom-header", equalTo("Custom value"));
+    }
+}
