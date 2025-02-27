80c65fc28c82ea4f261e68e9219fbc8fc6d972aa
dmitry-mak <akkaru@gmail.com>
2025-02-27 17:55:22 +0200

    Tests fail

--- src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
+++ src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Some data"));
+                .body("data", equalTo("data"));
     }
 
     @Test
